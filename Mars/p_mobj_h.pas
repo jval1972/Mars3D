6f9e8232f6c4e671ee47821fe1fd9c317840f119
jval1972 <jvalavanis@gmail.com>
2021-10-22 17:39:41 +0300

    Added MF3_EX_ABSOLUTEDAMAGE flag

--- Mars/p_mobj_h.pas
+++ Mars/p_mobj_h.pas
@@ -385,6 +385,8 @@ const
   MF3_EX_NOTAUTOAIMED = $400000;
   // Slide along walls alias
   MF3_EX_SLIDEONWALLS = $800000;
+  // Missiles use alternate damage (Strife)
+  MF3_EX_ABSOLUTEDAMAGE = $1000000;
 
 type
 // Map Object definition.
