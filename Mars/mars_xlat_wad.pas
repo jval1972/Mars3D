a6607123847a6a4ad94ab84c6527a88e2bdfe17d
jval1972 <jvalavanis@gmail.com>
2021-10-31 13:47:39 +0200

    Intermission music in xlat

--- Mars/mars_xlat_wad.pas
+++ Mars/mars_xlat_wad.pas
@@ -554,6 +554,7 @@ begin
   _convert_music_track(5, 'D_E1M5');
   _convert_music_track(6, 'D_E1M6');
   _convert_music_track(7, 'D_E1M7');
+  _convert_music_track(14, 'D_INTER');
 
   wadwriter.AddSeparator('M_END');
 
