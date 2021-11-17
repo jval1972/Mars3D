a8210697309e42fd17a29e6c79eaaf11126942f7
jval1972 <jvalavanis@gmail.com>
2021-11-17 20:35:14 +0200

    eset extra player fields when spawing player

--- Mars/p_mobj.pas
+++ Mars/p_mobj.pas
@@ -1298,6 +1298,22 @@ begin
   p.extralight := 0;
   p.fixedcolormap := 0;
   p.viewheight := PVIEWHEIGHT;
+  // JVAL: 20211117 - Reset extra player fields when spawing player
+  p.lastbreath := 0;
+  p.hardbreathtics := 0;
+  p.angletargetticks := 0;
+  p.laddertics := 0;
+  p.slopetics := 0;
+  p.teleporttics := 0;
+  p.nextoof := 0;
+  p.quakeintensity := 0;
+  p.quaketics := 0;
+  p.pcrosstic := 0;
+  p.jetpacksoundtic := 0;
+  p.underwatertics := 0;
+  p.damagetype := 0;
+  p.nextunderwaterportaltic := 0;
+  p.thrustmomz := 0;
 
   // setup gun psprite
   P_SetupPsprites(p);
