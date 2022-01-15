2a582fc727b371e0b1d616214f76a1db66d42009
jval1972 <jvalavanis@gmail.com>
2022-01-15 03:38:19 +0200

    Fixed intermission screen kill percentage

--- Mars/mars_intermission.pas
+++ Mars/mars_intermission.pas
@@ -5,7 +5,7 @@
 //  Copyright (C) 1997 by Engine Technology CO. LTD
 //  Copyright (C) 1993-1996 by id Software, Inc.
 //  Copyright (C) 2018 by Retro Fans of Mars3D
-//  Copyright (C) 2004-2021 by Jim Valavanis
+//  Copyright (C) 2004-2022 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -19,7 +19,7 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
-//  Foundation, inc., 59 Temple Place - Suite 330, Boston, MA
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //  02111-1307, USA.
 //
 //  DESCRIPTION:
@@ -424,7 +424,7 @@ begin
         if in_struct.maxkills = 0 then
           s := 'None'
         else
-          s := itoa(in_struct.plyr[in_struct.pnum].skills div in_struct.maxkills) + '%';
+          s := itoa((in_struct.plyr[in_struct.pnum].skills * 100) div in_struct.maxkills) + '%';
         _write_line(96, s);
         if _check_stage(IN_STAGE_STATS4) then
         begin
--- readme.txt
+++ readme.txt
@@ -19,7 +19,7 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program; if not, write to the Free Software
-//  Foundation, inc., 59 Temple Place - Suite 330, Boston, MA
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //  02111-1307, USA.
 //
 //------------------------------------------------------------------------------
@@ -96,6 +96,7 @@ Infinite state cycle detection while moving player sprites.
 Painchance actor field available in evaluated actor parameters.
 Use sound files in pk3 without WAD equivalent. Supported file formats are WAV, OGG, FLAC, OGA, AU, VOC & SND.
 Small optimizations and corrections to voxel software rendering.
+Fixed intermission screen kill percentage.
 
 Version 1.0.9.746 (20211226)
 -----------------
