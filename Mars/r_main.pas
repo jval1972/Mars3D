6c5689e02a657a023b0cd48e114f3978f0677f79
jval1972 <jvalavanis@gmail.com>
2022-03-18 12:55:16 +0200

    Fixed seg rendering in lines from (0, 0) point

--- Mars/r_main.pas
+++ Mars/r_main.pas
@@ -1058,9 +1058,6 @@ end;
 //==============================================================================
 function R_PointToAngleEx1(x: fixed_t; y: fixed_t): angle_t;
 begin
-  x := x - viewx;
-  y := y - viewy;
-
   if (x = 0) and (y = 0) then
   begin
     result := 0;
@@ -1157,6 +1154,9 @@ var
 //==============================================================================
 function R_PointToAngleEx(x: fixed_t; y: fixed_t): angle_t;
 begin
+  x := x - viewx;
+  y := y - viewy;
+
   if x = pta_x then
     if y = pta_y then
     begin
