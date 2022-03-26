e7687ff247b501195af72f3e504ea0f85701ff58
jval1972 <jvalavanis@gmail.com>
2022-03-26 18:35:47 +0200

    Optimization to multithreading flats software rendering

--- Base/r_flat8.pas
+++ Base/r_flat8.pas
@@ -122,6 +122,7 @@ implementation
 
 uses
   i_system,
+  i_threads,
   mt_utils,
   r_draw,
   r_main,
@@ -206,26 +207,63 @@ end;
 const
   MAXFLATRENDERINGTHREADS8 = 16;
 
+type
+  Pflatthreadparams8_t = ^flatthreadparams8_t;
+  flatthreadparams8_t = record
+    start, stop: integer;
+    next: Pflatthreadparams8_t;
+  end;
+
+var
+  R: array[0..MAXFLATRENDERINGTHREADS8 - 1] of flatthreadparams8_t; // JVAL: 20220320 - Made global
+
 //==============================================================================
 //
 // _flat_thread_worker8
 //
 //==============================================================================
-procedure _flat_thread_worker8(const p: pointer); stdcall;
+function _flat_thread_worker8(parms: Pflatthreadparams8_t): integer; stdcall;
 var
-  item1, item2: Pflatrenderinfo8_t;
+  item: Pflatrenderinfo8_t;
+  start, stop, part: integer;
+  i: integer;
 begin
-  item1 := @flatcache8[mt_range_p(p).start];
-  item2 := @flatcache8[mt_range_p(p).finish];
-  while integer(item1) <= integer(item2) do
+  while parms.start <= parms.stop do
   begin
-    item1.func(item1);
-    inc(item1);
+    item := @flatcache8[parms.start];
+    item.func(item);
+    ThreadInc(parms.start);
   end;
-end;
 
-var
-  R: array[0..MAXFLATRENDERINGTHREADS8 - 1] of mt_range_t;  // JVAL: 20220320 - Made global
+  // No further operations in main thread
+  if parms = @R[0] then
+  begin
+    Result := 0;
+    Exit;
+  end;
+
+  while true do
+  begin
+    parms := parms.next;
+    start := parms.start;
+    stop := parms.stop;
+    part := (stop - start) div 2;
+    if part > 2 then
+    begin
+      ThreadSet(parms.stop, parms.stop - part);
+      start := parms.stop + 1;
+      for i := start to stop do
+      begin
+        item := @flatcache8[i];
+        item.func(item);
+      end;
+    end
+    else if part < 1 then
+      Break;
+  end;
+
+  result := 0;
+end;
 
 //==============================================================================
 //
@@ -267,7 +305,8 @@ begin
   if flatcachesize8 < numthreads then
   begin
     R[0].start := 0;
-    R[0].finish := flatcachesize8 - 1;
+    R[0].stop := flatcachesize8 - 1;
+    R[0].next := @R[0];
     _flat_thread_worker8(@R[0]);
     flatcachesize8 := 0;
     exit;
@@ -278,8 +317,12 @@ begin
   for i := 1 to numthreads - 1 do
     R[i].start := Round(step * i);
   for i := 0 to numthreads - 2 do
-    R[i].finish := R[i + 1].start - 1;
-  R[numthreads - 1].finish := flatcachesize8 - 1;
+    R[i].stop := R[i + 1].start - 1;
+  R[numthreads - 1].stop := flatcachesize8 - 1;
+
+  for i := 0 to numthreads - 2 do
+    R[i].next := @R[i + 1];
+  R[numthreads - 1].next := @R[0];
 
   case numthreads of
    2:
@@ -472,15 +515,15 @@ end;
 // _flat3D_thread_worker8
 //
 //==============================================================================
-procedure _flat3D_thread_worker8(const p: pointer); stdcall;
+function _flat3D_thread_worker8(parms: Pflatthreadparams8_t): integer; stdcall;
 var
   item1, item2: Pflatrenderinfo8_t;
   start, finish: integer;
 begin
   item1 := @flatcache8[0];
   item2 := @flatcache8[flatcachesize8 - 1];
-  start := mt_range_p(p).start;
-  finish := mt_range_p(p).finish;
+  start := parms.start;
+  finish := parms.stop;
   while integer(item1) <= integer(item2) do
   begin
     if item1.ds_y >= start then
@@ -488,6 +531,8 @@ begin
         item1.func(item1);
     inc(item1);
   end;
+
+  Result := 0;
 end;
 
 //==============================================================================
@@ -530,7 +575,8 @@ begin
   if viewheight < numthreads then
   begin
     R[0].start := 0;
-    R[0].finish := viewheight - 1;
+    R[0].stop := viewheight - 1;
+    R[0].next := @R[0];
     _flat3D_thread_worker8(@R[0]);
     flatcachesize8 := 0;
     exit;
@@ -541,8 +587,12 @@ begin
   for i := 1 to numthreads - 1 do
     R[i].start := Round(step * i);
   for i := 0 to numthreads - 2 do
-    R[i].finish := R[i + 1].start - 1;
-  R[numthreads - 1].finish := viewheight - 1;
+    R[i].stop := R[i + 1].start - 1;
+  R[numthreads - 1].stop := viewheight - 1;
+
+  for i := 0 to numthreads - 2 do
+    R[i].next := @R[i + 1];
+  R[numthreads - 1].next := @R[0];
 
   case numthreads of
    2:
