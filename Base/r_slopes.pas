b410ecde5e98c29ff66946c848a2bb8265643a8e
jval1972 <jvalavanis@gmail.com>
2022-04-05 05:39:02 +0300

    Small optimization to plane drawing

--- Base/r_slopes.pas
+++ Base/r_slopes.pas
@@ -333,10 +333,9 @@ begin
   if x2 - x1 < 0 then
     exit;
 
-  if usefake3d and zaxisshift then
-    if fake3dspanpresent <> nil then
-      if not fake3dspanpresent[y] then
-        Exit;
+  if checkfake3dspanpresent then
+    if not fake3dspanpresent[y] then
+      Exit;
 
   if y < visslope.miny then
     yidx := visslope.miny
@@ -479,10 +478,9 @@ begin
   if x2 - x1 < 0 then
     exit;
 
-  if usefake3d and zaxisshift then
-    if fake3dspanpresent <> nil then
-      if not fake3dspanpresent[y] then
-        Exit;
+  if checkfake3dspanpresent then
+    if not fake3dspanpresent[y] then
+      Exit;
 
   if y < visslope.miny then
     yidx := visslope.miny
@@ -656,10 +654,9 @@ begin
   if x2 - x1 < 0 then
     exit;
 
-  if usefake3d and zaxisshift then
-    if fake3dspanpresent <> nil then
-      if not fake3dspanpresent[y] then
-        Exit;
+  if checkfake3dspanpresent then
+    if not fake3dspanpresent[y] then
+      Exit;
 
   if y < visslope.miny then
     yidx := visslope.miny
