d87da616cc60fa867c4a7d2268472485c5a509ef
jval1972 <jvalavanis@gmail.com>
2022-04-07 13:31:48 +0300

    No conflict for window position in OpenGL & software

--- Mars/m_defs.pas
+++ Mars/m_defs.pas
@@ -110,8 +110,10 @@ const
 var
 {$IFDEF OPENGL}
 // Stub variables
-  soft_SCREENWIDTH,
+  soft_SCREENWIDTH: integer;
   soft_SCREENHEIGHT: integer;
+  soft_windowxpos: integer;
+  soft_windowypos: integer;
   usefake3d: boolean;
   optimizedthingsrendering: boolean;
   force_numwallrenderingthreads_8bit: integer;
@@ -131,6 +133,8 @@ var
   r_lightmaponmasked: boolean = true;
   r_lightmaponemitters: boolean = false;
 {$ELSE}
+  gl_windowxpos: integer;
+  gl_windowypos: integer;
   tran_filter_pct: integer;
   use_fog: boolean;
   fog_density: integer;
@@ -177,7 +181,7 @@ type
   Pdefault_t = ^default_t;
 
 const
-  NUMDEFAULTS = 230;
+  NUMDEFAULTS = 232;
 
 // JVAL
 // Note: All setable defaults must be in lowercase, don't ask why. Just do it. :)
@@ -214,6 +218,22 @@ const
      defaultbvalue: true;
      _type: tInteger),
 
+    (name: 'soft_windowxpos';
+     location: {$IFDEF OPENGL}@soft_windowxpos{$ELSE}@windowxpos{$ENDIF};
+     setable: DFS_NEVER;
+     defaultsvalue: '';
+     defaultivalue: 0;
+     defaultbvalue: true;
+     _type: tInteger),
+
+    (name: 'soft_windowypos';
+     location: {$IFDEF OPENGL}@soft_windowypos{$ELSE}@windowypos{$ENDIF};
+     setable: DFS_NEVER;
+     defaultsvalue: '';
+     defaultivalue: 0;
+     defaultbvalue: true;
+     _type: tInteger),
+
     (name: 'gl_screenwidth';
      location: @{$IFDEF OPENGL}SCREENWIDTH{$ELSE}gl_SCREENWIDTH{$ENDIF};
      setable: DFS_NEVER;
@@ -238,16 +258,16 @@ const
      defaultbvalue: true;
      _type: tBoolean),
 
-    (name: 'windowxpos';
-     location: @windowxpos;
+    (name: 'gl_windowxpos';
+     location: {$IFDEF OPENGL}@windowxpos{$ELSE}@gl_windowxpos{$ENDIF};
      setable: DFS_NEVER;
      defaultsvalue: '';
      defaultivalue: 0;
      defaultbvalue: true;
      _type: tInteger),
 
-    (name: 'windowypos';
-     location: @windowypos;
+    (name: 'gl_windowypos';
+     location: {$IFDEF OPENGL}@windowypos{$ELSE}@gl_windowypos{$ENDIF};
      setable: DFS_NEVER;
      defaultsvalue: '';
      defaultivalue: 0;
